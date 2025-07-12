a73154caf1fb8896b1bb4cad44d0cc81b17fac50
tauradavin <222313401@stis.ac.id>
2025-07-13 02:38:30 +0700

    Update CORS to include new frontend domain

--- config/cors.php
+++ config/cors.php
@@ -7,6 +7,7 @@ return [
         'http://localhost:5173',
         'https://web-bps-frontend.vercel.app',
         'https://web-bps-frontend-inky.vercel.app',
+        'https://web-bps-frontend-vert.vercel.app', // ✅ domain baru ditambahkan
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
