193c53dbb67b298597a35aa6e71b7f1039db9879
tauradavin <222313401@stis.ac.id>
2025-07-13 09:52:23 +0700

    edit cors

--- config/cors.php
+++ config/cors.php
@@ -7,7 +7,7 @@ return [
         'http://localhost:5173',
         'https://web-bps-frontend-oabz.vercel.app/'
     ],
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['https://.*\.vercel\.app'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
